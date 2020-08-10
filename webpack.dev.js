34b4f0318b32fd3aea2861f55255cca09a17f340
heyitaki <akshath@berkeley.edu>
2020-08-10 01:18:55 -0700

    removing more boilerplate stuff

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const ExtensionReloader  = require('webpack-extension-reloader');
+const ExtensionReloader = require('webpack-extension-reloader');
 
 module.exports = merge(common, {
   mode: 'development',
