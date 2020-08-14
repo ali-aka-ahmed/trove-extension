803a4d825d1551243007fdd22b5dfabc2dc3d80a
heyitaki <akshath@berkeley.edu>
2020-08-14 11:30:24 -0700

    menu returns to side after drag

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,18 +1,36 @@
-import React from 'react';
-import Draggable from 'react-draggable';
+import React, { useState } from 'react';
+import Draggable, { DraggableData, DraggableEvent } from 'react-draggable';
 import { users } from '../../data';
 import Bubble from './bubble';
 import './index.scss';
 
 export default function Sidebar() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const SIDEBAR_WIDTH = 50;
+
+  const onDragStop = (event: DraggableEvent, data: DraggableData) => {
+    const height = window.innerHeight || document.documentElement.clientHeight;
+    const width = window.innerWidth || document.documentElement.clientWidth;
+
+    const leftDx = data.x - 0;
+    const rightDx = width - (data.x + SIDEBAR_WIDTH);
+
+    if (leftDx < rightDx) {
+      setPosition({ x: 0, y: data.y });
+    } else {
+      setPosition({ x: width - SIDEBAR_WIDTH, y: data.y });
+    }
+  };
+  
   const bubbles = users.map((sd) => <Bubble key={sd.id} />);
 
   return (
     <Draggable
       handle=".TbdBubble"
-      defaultPosition={{x: 0, y: 0}}
+      position={position}
+      onStop={onDragStop}
     >
-      <div className={`TbdSidebar`}>
+      <div className="TbdSidebar">
         {bubbles}
       </div>
     </Draggable>
