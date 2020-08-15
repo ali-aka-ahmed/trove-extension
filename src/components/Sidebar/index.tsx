0194389ff92424a15871e6d2c5e4a7fe1a88ce53
heyitaki <akshath@berkeley.edu>
2020-08-14 18:45:04 -0700

    bottom margin works based on height calc

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -9,18 +9,24 @@ export default function Sidebar() {
   const SIDEBAR_WIDTH = 50;
   const SIDEBAR_MARGIN = 15;
   const BUBBLE_HEIGHT = 50;
+  const BUBBLE_MARGIN = 7;
   
   const [position, setPosition] = useState({ x: SIDEBAR_MARGIN, y: SIDEBAR_MARGIN });
 
   // Get list of bubbles from dummy data
   const bubbles = users.map((sd) => <Bubble key={sd.id} user={sd} />);
 
+  function getHeight() {
+    if (bubbles.length === 0) return 0;
+    return bubbles.length * BUBBLE_HEIGHT + (bubbles.length - 1) * BUBBLE_MARGIN;
+  }
+
   /**
    * Return sidebar to closest edge of screen after being dragged.
-   * @param event 
+   * @param event   
    * @param data 
    */
-  function onDragStop(event: DraggableEvent, data: DraggableData) { console.log('hi')
+  function onDragStop(event: DraggableEvent, data: DraggableData) {
     // Calc screen bounds
     const height = window.innerHeight || document.documentElement.clientHeight;
     const width = window.innerWidth || document.documentElement.clientWidth;
@@ -31,7 +37,7 @@ export default function Sidebar() {
 
     // Set new position on closest edge
     const minY = SIDEBAR_MARGIN;
-    const maxY = height - bubbles.length * BUBBLE_HEIGHT - SIDEBAR_MARGIN;
+    const maxY = height - getHeight() - SIDEBAR_MARGIN;
     const newY = Math.min(Math.max(minY, data.y), maxY);
     if (leftDx < rightDx) {
       setPosition({ x: SIDEBAR_MARGIN, y: newY });
