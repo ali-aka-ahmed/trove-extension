ad71230c37df0fb37e3342d8867dd57ee56d7cbf
Ali Ahmed <ali.ahmed@berkeley.edu>
2020-08-19 10:46:05 -0700

    comments model completed

--- src/models/Comment.ts
+++ src/models/Comment.ts
@@ -1,14 +1,20 @@
+import CommentType from '../enums/CommentType';
+
 interface Like {
   id: string;
-  userId: string;
-  nickname: string;
+  name?: string;
+  userId?: string;
+  username?: string;
 };
 
 export default interface Comment {
   id: number;
-  userId: string;
-  content: string;
-  refId?: string;
-  likes: Like[];
-  numComments: number;
+  parentId?: string; // CommentType.Post, this does not exist
+  comments?: Comment[]; // CommentType.Replies, this does not exist
+  content?: string;
+  likes?: Like[];
+  name?: string;
+  type?: CommentType
+  userId?: string;
+  username?: string;
 };
