83afe376fd7edafc7d718a28c1411f19074c0c37
Ali Ahmed <ali.ahmed@berkeley.edu>
2020-08-19 19:01:53 -0700

    updated models that still need work

--- src/models/Comment.ts
+++ src/models/Comment.ts
@@ -1,20 +1,46 @@
-import CommentType from '../enums/CommentType';
+// interface Like {
+//   id: string;
+//   name?: string;
+//   userId?: string;
+//   username?: string;
+// };
 
-interface Like {
-  id: string;
-  name?: string;
-  userId?: string;
-  username?: string;
-};
-
-export default interface Comment {
+export interface Post {
   id: number;
-  parentId?: string; // CommentType.Post, this does not exist
-  comments?: Comment[]; // CommentType.Replies, this does not exist
+  comments?: Comment[];
   content?: string;
-  likes?: Like[];
+  // likes?: Like[];
   name?: string;
-  type?: CommentType
   userId?: string;
   username?: string;
+  taggedUserIds?: string[];
+  path?: string;
+  domain?: string;
+  url?: string;
+  // locations in here (url) (position on page) -- determine best way to calculate and then store
 };
+
+export interface Reply {
+  id: number;
+  parentId?: string;
+  content?: string;
+  name?: string;
+  userId?: string;
+  username?: string;
+  taggedUserIds?: string[];
+};
+
+export interface Comment {
+  id: number;
+  parentId?: string; 
+  replies?: Reply[];
+  content?: string;
+  name?: string;
+  userId?: string;
+  username?: string;
+  taggedUserIds?: string[];
+};
+
+// mongo check retrieving pattern (how to reduce server calls. retrive all the posts and comments and replies that we want to show)
+// get top level posts
+// perceived loading time is more important - soft load like fading in
