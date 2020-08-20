c258c9a5fab4c301c3a6adb005169a8a92b3259c
heyitaki <akshath@berkeley.edu>
2020-08-19 18:52:12 -0700

    comment out sidebar so it doesnt appear for now

--- src/components/content/index.tsx
+++ src/components/content/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ErrorBoundary from './ErrorBoundary';
 import './index.scss';
-import Sidebar from './sidebar';
-import bubbleStyles from './sidebar/bubble/index.scss';
-import sidebarStyles from './sidebar/index.scss';
+// import Sidebar from './sidebar';
+// import bubbleStyles from './sidebar/bubble/index.scss';
+// import sidebarStyles from './sidebar/index.scss';
 
 export default function Content() {
   return (
     <ErrorBoundary>
-      <Sidebar />
+      {/* <Sidebar />
       <style type="text/css">{sidebarStyles}</style>
-      <style type="text/css">{bubbleStyles}</style>
+      <style type="text/css">{bubbleStyles}</style> */}
     </ErrorBoundary>
   );
 }
