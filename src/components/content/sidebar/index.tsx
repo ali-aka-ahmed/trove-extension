cb0f43b291aa4b4aeb751a121f87870344d840b0
heyitaki <akshath@berkeley.edu>
2020-09-06 10:33:46 -0700

    debugging sidebar width

--- src/components/content/sidebar/index.tsx
+++ src/components/content/sidebar/index.tsx
@@ -1,31 +1,32 @@
+import { Tabs } from 'antd';
 import React, { useState } from 'react';
 import Draggable, { DraggableData, DraggableEvent } from 'react-draggable';
 import { getScrollbarDx } from '../../../utils/measurements';
 import Edge from './Edge';
 
-export const SIDEBAR_WIDTH = 60;
 export const SIDEBAR_MARGIN = 15;
 export const SIDEBAR_MARGIN_Y = 100;
-export const BUBBLE_HEIGHT = 60;
-export const BUBBLE_MARGIN = 25;
+export const BUBBLE_HEIGHT = 55;
+export const BUBBLE_MARGIN = 20;
+export const CONTENT_WIDTH = 300;
 
 export default function Sidebar() {
   const [position, setPosition] = useState({ x: SIDEBAR_MARGIN, y: SIDEBAR_MARGIN_Y });
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
   const [dragged, setDragged] = useState(false);
-  const [anchorEdge, setAnchorEdge] = useState(Edge.Left);
+  const [closestEdge, setClosestEdge] = useState(Edge.Left);
 
-  function getHeight() {
+  function getSidebarHeight() {
     return BUBBLE_HEIGHT;
+  }
+
+  function getSidebarWidth() {
+    return isOpen ? CONTENT_WIDTH : BUBBLE_HEIGHT;
   }
 
   function onClick(event: React.MouseEvent) {
     event.preventDefault();
     event.stopPropagation();
-    // if (!dragged) {
-      setIsOpen(!isOpen);
-    // }
-
     setDragged(false);
   }
 
@@ -49,29 +50,37 @@ export default function Sidebar() {
   function onDragStop(event: DraggableEvent, data: DraggableData) {
     event.preventDefault();
     event.stopPropagation();
-
+console.log(dragged, isOpen)
+    if (!dragged) {
+      const temp = !isOpen;
+      console.log(dragged, isOpen, temp)
+      setIsOpen(temp);
+    }
+console.log('ended', isOpen)
     // Calc screen bounds
     const height = window.innerHeight || document.documentElement.clientHeight;
     const width = (window.innerWidth - getScrollbarDx()) || document.documentElement.clientWidth;
 
     // Calc distance to each edge
     const leftDx = data.x - 0;
-    const rightDx = width - (data.x + SIDEBAR_WIDTH);
+    const rightDx  = width - (data.x + getSidebarWidth());
 
     // Set new position on closest edge
     const minY = SIDEBAR_MARGIN;
-    const maxY = height - getHeight() - SIDEBAR_MARGIN;
+    const maxY = height - getSidebarHeight() - SIDEBAR_MARGIN;
     const newY = Math.min(Math.max(minY, data.y), maxY);
     if (leftDx < rightDx) {
       setPosition({ x: SIDEBAR_MARGIN, y: newY });
-      setAnchorEdge(Edge.Left);
-    } else {
-      const newX = width - SIDEBAR_WIDTH - SIDEBAR_MARGIN;
+      setClosestEdge(Edge.Left);
+    } else { console.log(isOpen, width, getSidebarWidth(), SIDEBAR_MARGIN)
+      const newX = width - getSidebarWidth() - SIDEBAR_MARGIN;
       setPosition({ x: newX, y: newY });
-      setAnchorEdge(Edge.Right);
+      setClosestEdge(Edge.Right);
     }
+    console.log('=======================')
   };
 
+  const positionClass = `TbdSidebar--position-${closestEdge === Edge.Left ? 'left' : 'right'}`;
   return (
     <Draggable
       handle=".TbdSidebar__LogoBubble"
@@ -80,7 +89,7 @@ export default function Sidebar() {
       onDrag={onDrag}
       onStop={onDragStop}
     >
-      <div className="TbdSidebar">
+      <div className={`TbdSidebar ${positionClass}`}>
         <div 
           className="TbdSidebar__LogoBubble"
           onClick={(e) => onClick(e)}
@@ -88,7 +97,13 @@ export default function Sidebar() {
         >
         </div>
         {isOpen && (
-          <div className="TbdSidebar__Content"></div>
+          <div className="TbdSidebar__MainContent">
+            <Tabs defaultActiveKey="1">
+              <Tabs.TabPane tab="notifications" key="1">
+                
+              </Tabs.TabPane>
+            </Tabs>
+          </div>
         )}
       </div>
     </Draggable>
