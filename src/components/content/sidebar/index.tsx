0b1ea115694137e4d1f583bd3812a8a9b3e2bc54
heyitaki <akshath@berkeley.edu>
2020-09-10 12:59:56 -0700

    move getscrollbardx back to component bc it is not currently shared code

--- src/components/content/sidebar/index.tsx
+++ src/components/content/sidebar/index.tsx
@@ -1,6 +1,5 @@
 import { Tabs } from 'antd';
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
-import { getScrollbarDx } from '../../../utils/measurements';
 import Edge from './Edge';
 import Point from './Point';
 
@@ -30,6 +29,41 @@ export default function Sidebar() {
   const getSidebarWidth = useCallback(() => {
     return isOpen ? CONTENT_WIDTH : BUBBLE_HEIGHT;
   }, [isOpen]);
+
+  /**
+   * Get width of vertical scrollbar.
+   * TODO: Can prob be faster, memoize per page.
+   */
+  const getScrollbarDx = () => {
+    const html = document.querySelector('html');
+    if (html) return window.innerWidth - html.offsetWidth;
+
+    // Check if scrollbar actually exists
+    const overflow = document.body.scrollHeight > document.body.clientHeight;
+    const computed = window.getComputedStyle(document.body, null);
+    const exists = computed.overflow === 'visible'
+      || computed.overflowY === 'visible'
+      || (computed.overflow === 'auto' && overflow)
+      || (computed.overflowY === 'auto' && overflow);
+    if (!exists) return 0;
+
+    // Fallback method: append hidden element, force scrollbar, and calc width
+    const scrollDiv = document.createElement('div');
+    const styles = {
+      width: '100px',
+      height: '100px',
+      overflow: 'scroll',
+      position: 'absolute',
+      top: '-9999px'
+    };
+    Object.assign(scrollDiv.style, styles);
+    document.body.appendChild(scrollDiv);
+
+    // Calc width and remove element
+    const scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
+    document.body.removeChild(scrollDiv);
+    return scrollbarWidth;
+  }
 
   const anchorSidebar = useCallback(() => {
     // Calc screen bounds
