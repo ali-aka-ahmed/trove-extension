8e09e57014c1bdec4ff61522c2427ca24e7ec0ea
heyitaki <akshath@berkeley.edu>
2020-09-10 22:51:10 -0700

    finish onchanged.addlistener

--- src/utils/chromeStorage.ts
+++ src/utils/chromeStorage.ts
@@ -1,6 +1,16 @@
 import Point from '../components/content/sidebar/Point';
 
 /**
+ * Key to type mapping.
+ */
+export interface CS {
+  extensionOn: boolean;
+  loggedIn: boolean;
+  sidebarOpen: boolean;
+  sidebarPosition: Point;
+}
+
+/**
  * Get values corresponding to given keys from local storage. This method takes in a single key, 
  * a list of keys, or an object containing the keys mapped to their default values, and returns a
  * promise which returns an object containing the key-value pairs retrieved from storage. `null` 
@@ -24,7 +34,7 @@ export function localGet<K extends keyof CS>(key: K | K[] | {[k in K]: CS[k]}) {
       const err = chrome.runtime.lastError;
       if (err) {
         console.error(`Failed to get ${key} from chrome.storage.local. Error: ${err.message}`);
-        reject(err.message);
+        reject(err);
       } else {
         resolve(items);
       }
@@ -42,7 +52,7 @@ export function localSet<K extends keyof CS>(items: {[k in K]: CS[k]}): Promise<
       const err = chrome.runtime.lastError;
       if (err) {
         console.error(`Failed to set ${items} to chrome.storage.local. Error: ${err.message}`);
-        reject(err.message);
+        reject(err);
       } else {
         resolve();
       }
@@ -60,7 +70,7 @@ export function localRemove<K extends keyof CS>(keys: K | K[]): Promise<void> {
       const err = chrome.runtime.lastError;
       if (err) {
         console.error(`Failed to remove ${keys} from chrome.storage.local. Error: ${err.message}`);
-        reject(err.message);
+        reject(err);
       } else {
         resolve();
       }
@@ -77,7 +87,7 @@ export function localClear(): Promise<void> {
       const err = chrome.runtime.lastError;
       if (err) {
         console.error(`Failed to clear chrome.storage.local. Error: ${err.message}`);
-        reject(err.message);
+        reject(err);
       } else {
         resolve();
       }
@@ -85,12 +95,16 @@ export function localClear(): Promise<void> {
   });
 }
 
-/**
- * Key to type mapping.
- */
-export interface CS {
-  extensionOn: boolean;
-  loggedIn: boolean;
-  sidebarPosition: Point;
-  sidebarOpen: boolean;
+// Not sure if we need this/if it works, will test 
+export function addListener() {
+  return new Promise((resolve, reject) => {
+    chrome.storage.onChanged.addListener((changes, areaName) => {
+      const err = chrome.runtime.lastError;
+      if (err) {
+        reject(err);
+      } else {
+        resolve({changes, areaName}); 
+      }
+    });
+  });
 }
