9d4c9174a47e719e2c2d9701cdc765dc9caf93c6
Ali Ahmed <ali.ahmed@berkeley.edu>
2020-09-10 23:05:43 -0700

    adjusted

--- src/components/popup/notification/index.tsx
+++ src/components/popup/notification/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Notification as INotification } from '../../../models';
 import { displayRelativeTime } from '../../../utils';
-import './style.scss';
 import '../style.scss';
+import './style.scss';
 
 interface NotificationProps {
   notification: INotification
@@ -11,9 +11,9 @@ interface NotificationProps {
 const Notification = ({ notification }: NotificationProps) => {
   
   const renderContent = (content: string) => {
-    const regex = new RegExp(`(${notification.taggedUsers?.map(user => `@${user.username}`).join('|')})`);
+    const regex = new RegExp(`(${notification.taggedUsers?.map(user => `@${user.username}`).join("|")})`);
     const tokenizedContent = content.split(regex).filter(str => !!str);
-    const isUsername = (str: string) => { return str[0] === '@' };
+    const isUsername = (str: string) => { return str[0] === "@" };
     const getColor = (tag: string) => { 
       return notification.taggedUsers?.find(user => user.username === tag.slice(1))?.color; 
     }
@@ -23,8 +23,8 @@ const Notification = ({ notification }: NotificationProps) => {
           <span 
             key={subString}
             className={isUsername(subString)
-              ? 'TbdNotificationContent--Username' 
-              : 'TbdNotificationContent--Normal'
+              ? "TbdNotificationContent--Username" 
+              : "TbdNotificationContent--Normal"
             }
             style={isUsername(subString) ? { color: getColor(subString) } : {}}
           >
@@ -36,28 +36,28 @@ const Notification = ({ notification }: NotificationProps) => {
   }
  
 	return (
-    <div className='TbdNotificationContainer'>
-      <div className='TbdNotificationContainer__HeaderWrapper'>
+    <div className="TbdNotificationContainer">
+      <div className="TbdNotificationContainer__HeaderWrapper">
         <div 
-          className='TbdProfile__Img'
+          className="TbdProfile__Img"
           style={{ backgroundColor: notification.sender.color }}
         />
-        <div className='TbdNotificationContainer__HeaderContentWrapper'>
-          <div className='TbdNotificationContainer__Notification'>
+        <div className="TbdNotificationContainer__HeaderContentWrapper">
+          <div className="TbdNotificationContainer__Notification">
             <span 
               style={{color: notification.sender.color}}
-              className='TbdNotificationContainer__DisplayName'
+              className="TbdNotificationContainer__DisplayName"
             >
               {`${notification.sender.displayName} `}
             </span>
             {notification.action}
           </div>
-          <div className='TbdNotificationContainer__NotificationDetails'>
+          <div className="TbdNotificationContainer__NotificationDetails">
             {`${displayRelativeTime(notification.creationDatetime)} · ${notification.url}`}
           </div>
         </div>
       </div>
-      <div className='TbdNotificationContainer__Content'>
+      <div className="TbdNotificationContainer__Content">
         {notification.content && renderContent(notification.content)}
       </div>
     </div>
