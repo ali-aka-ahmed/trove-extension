5ba262f2231cdeb2aa1218528fb9f96b846cad24
Ali Ahmed <ali.ahmed@berkeley.edu>
2020-09-10 23:17:19 -0700

    remove server route prefixes (not a fan, but we can always easily change this

--- src/server/index.ts
+++ src/server/index.ts
@@ -8,28 +8,28 @@ const api_inst = axios.create({
 
 // export const rSignup = async () => {};
 
-export const rLogin = () => {
+export const login = () => {
     // const responseData = await api_inst.post('/login', {username, password});
     // return responseData.data;
     // return users.find(user => user.username === 'ali')
 };
 
-export const rLogout = async () => {
+export const logout = async () => {
     const responseData = await api_inst.get('/logout');
     return responseData.data;
 };
 
-export const rCheckValidUsername = async (username: string) => {
+export const checkValidUsername = async (username: string) => {
     const responseData = await api_inst.post('/check-valid-username', {username});
     return responseData.data;
 };
 
-export const rForgotPassword = async (email: string) => {
+export const forgotPassword = async (email: string) => {
     const responseData = await api_inst.post('/forgot-password', {email});
     return responseData.data;
 };
 
-export const rResetPassword = async (password: string, token: string) => {
+export const resetPassword = async (password: string, token: string) => {
     const responseData = await api_inst.post(`/reset/${token}`, {password});
     return responseData.data;
 };
