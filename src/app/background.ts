e464cd4a92bf7db07d8301275a327e48083284b0
Ali Ahmed <ali.ahmed@berkeley.edu>
2020-09-12 11:32:04 -0700

    added comp library, added mixins

--- src/app/background.ts
+++ src/app/background.ts
@@ -5,31 +5,31 @@ import { users } from '../utils/data';
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   // onMessage must return 'true' if response is async.
   let isResponseAsync = false;
-
-  if (request.popupMounted) {
-    console.log('eventPage notified that Popup.tsx has mounted.');
-  }
-
+  if (request.popupMounted) console.log('eventPage notified that Popup.tsx has mounted.');
   return isResponseAsync;
 });
 
 chrome.runtime.onStartup.addListener(async () => {
-  console.log('onStartup')
+  console.log('onStartup');
+
   // Check if user token exists
-  const userTokenExists = true;
+
   // If it does, then log the user in and fetch their details (auth = true)
-  const userDetails = users.find(user => user.id === 'fce65bd0-8af5-4504-a19d-8cbc767693f7')!
-  await localSet({ user: userDetails })
+  const userDetails = users.find(user => user.id === 'fce65bd0-8af5-4504-a19d-8cbc767693f7')!;
+  await localSet({ user: userDetails, authenticated: true });
+
   // If not, then show signup page (auth = false)
 })
 
 // Extension installed or updated
 chrome.runtime.onInstalled.addListener(async () => {
-  console.log('onInstalled')
+  console.log('onInstalled');
+
   // Check if user token exists
-  const userTokenExists = true;
+
   // If it does, then log the user in and fetch their details (auth = true)
-  const userDetails = users.find(user => user.id === 'fce65bd0-8af5-4504-a19d-8cbc767693f7')!
-  await localSet({ user: userDetails })
+  const userDetails = users.find(user => user.id === 'fce65bd0-8af5-4504-a19d-8cbc767693f7')!;
+  await localSet({ user: userDetails, authenticated: true });
+
   // If not, then show signup page (auth = false)
 })
