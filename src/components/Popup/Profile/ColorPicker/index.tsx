5287d7ec0eff7113830b175779e576a29470239a
Ali Ahmed <ali.ahmed@berkeley.edu>
2020-09-12 16:33:33 -0700

    everything done down to server route - just missing socket to connect notifications

--- src/components/Popup/Profile/ColorPicker/index.tsx
+++ src/components/Popup/Profile/ColorPicker/index.tsx
@@ -3,65 +3,52 @@ import '../style.scss';
 import './style.scss';
 
 interface ColorPickerProps {
-  currColor: string;
   onSelect: ( (hex: string) => Promise<void> | void );
 }
 
-const ColorPicker = ({ currColor, onSelect }: ColorPickerProps) => {
+const ColorPicker = ({ onSelect }: ColorPickerProps) => {
 	return (
     <div className="TbdColorPicker">
-      <div className="TbdColorPicker__TopRow">
-        <div 
-          className="TbdColorPicker__ColorOption"
-          style={{color: '#FF6900'}} 
-          onClick={() => onSelect('#FF6900')}
-        />
-        <div 
-          className="TbdColorPicker__ColorOption"
-          style={{color: '#FCB900'}} 
-          onClick={() => onSelect('#FCB900')}
-        />
-        <div 
-          className="TbdColorPicker__ColorOption"
-          style={{color: '#7BDCB5'}} 
-          onClick={() => onSelect('#7BDCB5')}
-        />
-        <div 
-          className="TbdColorPicker__ColorOption"
-          style={{color: '#00D084'}} 
-          onClick={() => onSelect('#00D084')}
-        />
-        <div
-          className="TbdColorPicker__ColorOption" 
-          style={{color: '#8ED1FC'}} 
-          onClick={() => onSelect('#8ED1FC')}
-        />
-        <div 
-          className="TbdColorPicker__ColorOption"
-          style={{color: '#0693E3'}} 
-          onClick={() => onSelect('#0693E3')}
-        />
-        <div 
-          className="TbdColorPicker__ColorOption"
-          style={{color: '#ABB8C3'}} 
-          onClick={() => onSelect('#ABB8C3')}
-        />
-        <div 
-          className="TbdColorPicker__ColorOption"
-          style={{color: '#EB144C'}} 
-          onClick={() => onSelect('#EB144C')}
-        />
-        <div 
-          className="TbdColorPicker__ColorOption"
-          style={{color: '#F78DA7'}} 
-          onClick={() => onSelect('#F78DA7')}
-        />
-        <div 
-          className="TbdColorPicker__ColorOption"
-          style={{color: '#9900EF'}} 
-          onClick={() => onSelect('#9900EF')}
-        />
-      </div>
+      <div 
+        className="TbdProfile__ColorPreview"
+        style={{backgroundColor: '#FF6900'}} 
+        onClick={() => onSelect('#FF6900')}
+      />
+      <div 
+        className="TbdProfile__ColorPreview"
+        style={{backgroundColor: '#FCB900'}} 
+        onClick={() => onSelect('#FCB900')}
+      />
+      <div 
+        className="TbdProfile__ColorPreview"
+        style={{backgroundColor: '#00D084'}} 
+        onClick={() => onSelect('#00D084')}
+      />
+      <div
+        className="TbdProfile__ColorPreview" 
+        style={{backgroundColor: '#8ED1FC'}} 
+        onClick={() => onSelect('#8ED1FC')}
+      />
+      <div 
+        className="TbdProfile__ColorPreview"
+        style={{backgroundColor: '#0693E3'}} 
+        onClick={() => onSelect('#0693E3')}
+      />
+      <div 
+        className="TbdProfile__ColorPreview"
+        style={{backgroundColor: '#EB144C'}} 
+        onClick={() => onSelect('#EB144C')}
+      />
+      <div 
+        className="TbdProfile__ColorPreview"
+        style={{backgroundColor: '#F78DA7'}} 
+        onClick={() => onSelect('#F78DA7')}
+      />
+      <div 
+        className="TbdProfile__ColorPreview"
+        style={{backgroundColor: '#9900EF'}} 
+        onClick={() => onSelect('#9900EF')}
+      />
     </div>
 	)
 };
--- src/components/popup/profile/index.tsx
+++ src/components/popup/profile/index.tsx
@@ -1,7 +1,11 @@
-import { EditOutlined } from '@ant-design/icons';
+import { EditOutlined, LoadingOutlined, SaveOutlined } from '@ant-design/icons';
 import React, { useState } from 'react';
 import { User } from '../../../models';
+import { updateColor, updateDisplayName, updateUsername } from '../../../server';
+import { validateDisplayName, validateUsername } from '../../../utils';
+import { localSet } from '../../../utils/chromeStorage';
 import '../style.scss';
+import ColorPicker from './ColorPicker';
 import './style.scss';
 
 interface ProfileProps {
@@ -9,40 +13,103 @@ interface ProfileProps {
 }
 
 export default function Profile({ user }: ProfileProps) {
-  const [showEditIcon, setShowEditIcon] = useState<'displayName' | 'username' | null>(null);
-
-  const [editable, setEditable] = useState<'displayName' | 'username' | null>(null);
+  const [showEditIcon, setShowEditIcon] = useState<'displayName' | 'username' | 'color' | null>(null);
+  const [editable, setEditable] = useState<'displayName' | 'username' | 'color' | null>(null);
+  const [loading, setLoading] = useState<'displayName' | 'username' | 'color' | null>(null);
 
   const [displayName, setDisplayName] = useState(user.displayName);
   const [username, setUsername] = useState(user.username);
-  const [color, setColor] = useState(user.color);
+
+  const [showError, setShowError] = useState<'displayName' | 'username' | 'color' | null>(null);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleEditProperty = (property: 'displayName' | 'username' | 'color') => {
+    if (editable !== null) return;
+    setEditable(property); 
+    setShowEditIcon(null);
+  }
+
+  const saveDisplayName = async () => {
+    setLoading('displayName');
+    const vRes = validateDisplayName(displayName)
+    if (!vRes.success) {
+      setShowError('displayName');
+      setErrorMessage(vRes.message || 'Please enter a name!');
+      setLoading(null);
+      return;
+    }
+    const res = await updateDisplayName(displayName);
+    if (!res.error && res.user) {
+      await localSet({ user: res.user });
+      setEditable(null);
+    } else if (res.error) {
+      setShowError('displayName');
+      setErrorMessage(res.error.message);
+    }
+    setLoading(null);
+  }
+
+  const saveUsername = async () => {
+    setLoading('username');
+    const vRes = validateUsername(username)
+    if (!vRes.success) {
+      setShowError('username');
+      setErrorMessage(vRes.message || 'Invalid username!');
+      setLoading(null);
+      return;
+    }
+    const res = await updateUsername(username);
+    if (!res.error && res.user) {
+      await localSet({ user: res.user });
+      setEditable(null);
+    } else if (res.error) {
+      setShowError('username');
+      setErrorMessage(res.error.message);
+    }
+    setLoading(null);
+  }
+
+  const saveColor = async (newColor: string) => {
+    setLoading('color');
+    const res = await updateColor(newColor);
+    if (!res.error && res.user) {
+      await localSet({ user: res.user });
+      setEditable(null);
+    } else if (res.error) {
+      setShowError('color');
+      setErrorMessage(res.error.message);
+    }
+    setLoading(null);
+  }
 
   return (
     <div className="TbdProfile__Wrapper">
       <div className="TbdProfile__Header">
-        <div 
+        <div
           className="TbdProfile__Img"
-          style={{ backgroundColor: color }} 
+          style={{ backgroundColor: user.color }} 
         >
           {displayName[0]}
         </div>
         <div className="TbdProfile__HeaderContent">
           {editable === 'displayName' ? (
-            <div 
-              onClick={() => { setEditable('displayName') }}
-              className="TbdProfile__DisplayName"
-            >
-              {displayName}
+            <div className="TbdProfile__InputWrapper">
+              <input
+                autoFocus
+                className="TbdProfile__Input TbdProfile__Input--display-name"
+                value={displayName} 
+                onChange={(e) => { setDisplayName(e.target.value) }}
+              />
               <div 
-                style={showEditIcon !== 'displayName' ? { opacity: 0 } : {}}
-                className="TbdProfile__EditIcon"
+                className="TbdProfile__SaveIcon TbdProfile__SaveIcon--display-name"
+                onClick={saveDisplayName}
               >
-                <EditOutlined />
+                {loading === 'displayName' ? <LoadingOutlined /> : <SaveOutlined />}
               </div>
             </div>
           ) : (
             <div 
-              onClick={() => { setEditable('displayName') }}
+              onClick={() => { handleEditProperty('displayName') }}
               onMouseEnter={() => { setShowEditIcon('displayName') }}
               onMouseLeave={() => { setShowEditIcon(null) }}
               className="TbdProfile__DisplayName"
@@ -56,32 +123,64 @@ export default function Profile({ user }: ProfileProps) {
               </div>
             </div>
           )}
-
-          <div
-            onClick={() => { setEditable('username') }}
-            onMouseEnter={() => { setShowEditIcon('username') }} 
-            onMouseLeave={() => { setShowEditIcon(null) }}
-            style={{ color }}
-            className="TbdProfile__Username"
-          >
-            {`@${username}`}
-            <div 
-              style={showEditIcon !== 'username' ? { opacity: 0 } : {}}
-              className="TbdProfile__EditIcon"
-            >
-              <EditOutlined />
+          {editable === 'username' ? (
+            <div className="TbdProfile__InputWrapper">
+              <div className="TbdProfile__InputPrefix" style={{ color: user.color }}>@</div>
+              <input
+                autoFocus
+                style={{ color: user.color }}
+                className="TbdProfile__Input TbdProfile__Input--username"
+                value={username} 
+                onChange={(e) => { setUsername(e.target.value) }}
+              />
+              <div 
+                className="TbdProfile__SaveIcon TbdProfile__SaveIcon--username"
+                onClick={saveUsername}
+              >
+                {loading === 'username' ? <LoadingOutlined /> : <SaveOutlined />}
+              </div>
             </div>
-          </div>
+          ) : (
+            <div
+              onClick={() => { handleEditProperty('username') }}
+              onMouseEnter={() => { setShowEditIcon('username') }} 
+              onMouseLeave={() => { setShowEditIcon(null) }}
+              style={{ color: user.color }}
+              className="TbdProfile__Username"
+            >
+              {`@${username}`}
+              <div 
+                style={showEditIcon !== 'username' ? { opacity: 0 } : {}}
+                className="TbdProfile__EditIcon"
+              >
+                <EditOutlined />
+              </div>
+            </div>
+          )}
         </div>
       </div>
-      <div className="TbdProfile__AccentColorWrapper">
-        <div className="TbdText">Accent Color</div>
-        <div 
-          onClick={() => {}}
+      {editable === 'color' ? (
+        <div className="TbdProfile__Color TbdProfile__InputWrapper">
+          <div className="TbdProfile__ColorText">Accent Color</div>
+          <ColorPicker onSelect={saveColor} />
+        </div>
+      ) : (
+        <div
+          onClick={() => { handleEditProperty('color') }}
+          onMouseEnter={() => { setShowEditIcon('color') }} 
+          onMouseLeave={() => { setShowEditIcon(null) }}
           className="TbdProfile__Color"
-          style={{ backgroundColor: color }}
-        />
-      </div>
+        >
+          <div className="TbdProfile__ColorText">Accent Color</div>
+          <div className="TbdProfile__ColorPreview" style={{ backgroundColor: user.color }} />
+          <div 
+            style={showEditIcon !== 'color' ? { opacity: 0 } : {}} 
+            className="TbdProfile__EditIcon"
+          >
+            <EditOutlined />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
