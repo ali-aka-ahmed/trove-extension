dbbcab89191cd2b8562564e185dbb63ab3361033
Ali Ahmed <ali.ahmed@berkeley.edu>
2020-09-13 12:30:15 -0700

    styling changes

---
+++ src/components/Popup/Notification/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Notification as INotification } from '../../../models';
+import { displayRelativeTime } from '../../../utils';
+import '../style.scss';
+import './style.scss';
+
+interface NotificationProps {
+  notification: INotification;
+}
+
+const Notification = ({ notification }: NotificationProps) => {
+  
+  const renderContent = (content: string) => {
+    const regex = new RegExp(`(${notification.taggedUsers?.map(user => `@${user.username}`).join('|')})`);
+    const tokenizedContent = content.split(regex).filter(str => !!str);
+    const isUsername = (str: string): boolean => { return str[0] === '@' };
+    const getUserColor = (tag: string): string | undefined => { 
+      return notification.taggedUsers?.find(user => user.username === tag.slice(1))?.color; 
+    }
+    return (
+      <>
+        {tokenizedContent.map((subString, i) => 
+          <span key={i} style={isUsername(subString) ? { color: getUserColor(subString) } : {}}>
+            {subString}
+          </span>
+        )}
+      </>
+    )
+  }
+ 
+	return (
+    <div className="TbdNotificationWrapper">
+      <div className="TbdNotificationWrapper__HeaderWrapper">
+        <div className="TbdProfile__Img" style={{ backgroundColor: notification.sender.color }}>
+          {notification.sender.displayName[0]}
+        </div>
+        <div className="TbdNotificationWrapper__HeaderContentWrapper">
+          <div className="TbdNotificationWrapper__Notification">
+            <span className="TbdNotificationWrapper__DisplayName">
+              {`${notification.sender.displayName} `}
+            </span>
+            <span className="TbdNotificationWrapper__Action">
+              {notification.action}
+            </span>
+          </div>
+          <div className="TbdNotificationWrapper__NotificationDetails">
+            {`${displayRelativeTime(notification.creationDatetime)} · ${notification.url}`}
+          </div>
+        </div>
+      </div>
+      <div className="TbdNotificationWrapper__Content">
+        {notification.content && renderContent(notification.content)}
+      </div>
+    </div>
+	)
+};
+
+export default Notification;
---
+++ src/components/Popup/index.tsx
@@ -0,0 +1,91 @@
+import { Switch, Tabs } from 'antd';
+import 'antd/dist/antd.min.css';
+import React, { useEffect, useState } from 'react';
+import { Notification as INotification, User as IUser } from '../../models';
+import { get, set } from '../../utils/chromeStorage';
+import { notifications as notificationData } from '../../utils/data';
+import Notification from './Notification';
+import Profile from './Profile';
+import './style.scss';
+
+function Popup() {
+  /**
+   * State for all components in Popup.
+   */
+  const [notifications, setNotifications] = useState<INotification[]>([]);
+
+  /**
+   * Global state.
+   */
+  const [extensionOn, setExtensionOn] = useState(false);
+  const [authenticated, setAuthenticated] = useState(false);
+  const [user, setUser] = useState<IUser | null>(null);
+
+  useEffect(() => {
+    get(null).then(items => {
+      if (items['extensionOn']) setExtensionOn(items['extensionOn'])
+      if (items['authenticated']) setAuthenticated(items['authenticated'])
+      if (items['user']) setUser(items['user'])
+    });
+    
+    chrome.storage.onChanged.addListener(changes => {
+      if (changes['extensionOn']) setExtensionOn(changes['extensionOn'].newValue)
+      if (changes['authenticated']) setAuthenticated(changes['authenticated'].newValue)
+      if (changes['user']) setUser(changes['user'].newValue)
+    });
+  }, [])
+
+  /**
+   * Establish socket to server to receive notifications.
+   */
+  useEffect(() => {
+    const getNotifications = async (): Promise<void> => {
+      const n = notificationData[0]
+      let notifs: INotification[] = []
+      for (let i=0; i<11; i++) {
+        let s: any = {}
+        s = Object.assign(s, n)
+        s.id = i.toString()
+        notifs.push(s)
+      }
+      setNotifications(notifs)
+    }
+    getNotifications();
+  }, [])
+
+  /**
+   * Turn extension on/off. Save to global state.
+   * @param checked New global on/off value.
+   */
+  const handleOnOff = async (checked: boolean) => {
+    await set({ extensionOn: checked })
+  }
+
+  return (
+    <div className="TbdPopupContainer">
+      <Tabs defaultActiveKey="1">
+        <Tabs.TabPane tab="notifications" key="1">
+          <div className="TbdPopupContainer__TabWrapper">
+            {notifications.map(n => (
+              <Notification key={n.id} notification={n} />
+            ))}
+          </div>
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="profile" key="2">
+          <div className="TbdPopupContainer__TabWrapper">
+            {user && <Profile user={user} />}
+          </div>
+        </Tabs.TabPane>
+      </Tabs>
+      <div className="TbdPopupContainer__OnOffWrapper">
+        <div className="TbdPopupContainer__OnOffTextWrapper">
+          <div>Turn Accord</div>
+          <div className="TbdPopupContainer__OnOff">{extensionOn ? 'OFF' : 'ON'}</div>
+        </div>
+        <Switch onClick={(checked) => { handleOnOff(checked) }} checked={extensionOn} />
+      </div>
+    </div>
+  );
+};
+
+export default Popup;
