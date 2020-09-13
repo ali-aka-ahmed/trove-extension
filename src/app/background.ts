d2fa5ce2a9e2f4a28a7e278ac9e981520b628d5a
heyitaki <akshath@berkeley.edu>
2020-09-12 17:35:35 -0700

    syncing working across windows

--- src/app/background.ts
+++ src/app/background.ts
@@ -1,17 +1,14 @@
 import { triggerSync } from "../components/content/sidebar/Syncer";
+import { getActiveTabs, Message } from "../utils/chrome/tabs";
 
-// Listen to messages sent from other parts of the extension.
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  // onMessage must return "true" if response is async.
-  let isResponseAsync = false;
-
-  if (request.popupMounted) {
-    console.log('eventPage notified that Popup.tsx has mounted.');
+// Listen to messages sent from other parts of the extension
+chrome.runtime.onMessage.addListener(async (message: Message, sender, sendResponse) => {
+  if (message.type.slice(0, 5) === 'sync.') {
+    triggerSync(await getActiveTabs(), message);
   }
-
-  return isResponseAsync;
 });
 
+// Listen on when a tab becomes active
 chrome.tabs.onActivated.addListener((activeInfo) => {
   console.log(`Tab ${activeInfo.tabId} active.`);
   triggerSync(activeInfo.tabId, ['isOpen', 'position']);
--- src/components/content/sidebar/Syncer.ts
+++ src/components/content/sidebar/Syncer.ts
@@ -1,5 +1,6 @@
 import { CS, get } from "../../../utils/chrome/storage";
 import { Message, sendMessage } from "../../../utils/chrome/tabs";
+import { toArray } from "../../../utils/general";
 
 type SetterMap = {[k in keyof Partial<CS>]: React.Dispatch<React.SetStateAction<CS[k]>>};
 
@@ -21,10 +22,33 @@ export default class Syncer {
   }
 }
 
+/**
+ * Enable content scripts to request extension to sync specified state.
+ * @param keys 
+ */
+export const requestSync = <K extends keyof CS>(keys: K | K[]) => {
+  const keysList = toArray(keys);
+  const type = `sync.${keysList.join('.')}`;
+  chrome.runtime.sendMessage({ type });
+}
+
+/**
+ * Enable extension to trigger sync for given tabs. Message may or may not already be constructed.
+ * @param tabs 
+ * @param message 
+ */
 export const triggerSync = <K extends keyof CS>(
   tabs: number | number[] | chrome.tabs.Tab | chrome.tabs.Tab[], 
-  keys: K[]
+  message: K | K[] | Message
 ) => {
-  const type = `sync.${keys.join('.')}`;
-  sendMessage(tabs, { type });
+  if (typeof message === 'string') {
+    message = [message];
+  }
+
+  if (typeof message[0] === 'string') {
+    const type = `sync.${(message as string[]).join('.')}`;
+    message = { type };
+  }
+  
+  sendMessage(tabs, message as Message);
 }
