47cb5a78b6396f52544a88bed0746697b270910a
heyitaki <akshath@berkeley.edu>
2020-09-14 16:10:27 -0700

    new post stuff shows on sidebar

--- src/components/Content/Sidebar/NewPost/index.tsx
+++ src/components/Content/Sidebar/NewPost/index.tsx
@@ -5,22 +5,51 @@ import { Post } from '../../../../models';
 import { get } from '../../../../utils/chrome/storage';
 import { getAnchor } from '../../helpers/anchor';
 
+const MAX_POST_LENGTH = 280;
+
 export default function NewPost() {
   const [isAnchoring, setIsAnchoring] = useState(false);
   const [post, setPost] = useState({} as Partial<Post>);
+
+  const canSubmit = useCallback(() => {
+    const cantSubmit = !post.anchor 
+      || !post.content 
+      || post.content.length === 0
+      || post.content.length > MAX_POST_LENGTH;
+    return !cantSubmit;
+  }, [post]);
+
+  const submit = useCallback(() => {
+    // TODO: compute tagged users
+    // TODO: make sure anchor was done on this url
+    setPost({
+      ...post,
+      url: '',
+      creationDatetime: Date.now()
+    });
+  }, [post]);
   
-  const handleClick = (e) => {
+  const clickAnchorButton = (e) => {
     console.log("HELP")
     setIsAnchoring(true);
   }
+
+  const clickSubmitButton = useCallback((e) => {
+    submit();
+  }, [submit]);
 
   const onClickPage = useCallback((e: MouseEvent) => {
     e.preventDefault();
     e.stopPropagation();
     console.info('comment: clickpage');
+    // TODO: anchor point cannot be on tbd elements
     
-    getAnchor(e);
-  }, []);
+    setPost({
+      ...post,
+      anchor: getAnchor(e)
+    });
+    setIsAnchoring(false);
+  }, [post]);
 
   useEffect(() => {
     if (isAnchoring) {
@@ -45,12 +74,16 @@ export default function NewPost() {
   }, []);
 
   return (
-    <div 
-      className="TbdNewComment"
-      onClick={handleClick} 
-    >
+    <div className="TbdNewComment">
       <p className="TbdNewComment__Text">fake text</p>
-      <Button type="primary" shape="circle">A</Button>
+      <Button 
+        type="primary" 
+        shape="circle" 
+        onClick={clickAnchorButton}
+      >
+        A
+      </Button>
+      <Button type="primary" onClick={clickSubmitButton}>POST</Button>
     </div>
   );
 }
