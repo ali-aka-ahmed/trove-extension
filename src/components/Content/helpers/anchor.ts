3d463ead90503529f0fd0d0ab96874db1cdb5217
heyitaki <akshath@berkeley.edu>
2020-09-14 12:35:32 -0700

    added anchoring helpers'

--- src/components/Content/helpers/anchor.ts
+++ src/components/Content/helpers/anchor.ts
@@ -1,4 +1,6 @@
-export const getAnchorPoint = () => {
+import Point from "./Point";
+
+export const getAnchorPoint = (e: React.MouseEvent | MouseEvent) => {
 
 }
 
@@ -8,4 +10,30 @@ export const parseAnchor = () => {
 
 export const parseAnchorPoint = () => {
 
+}
+
+export const getScrollTop = (): number => {
+  return window.pageYOffset 
+    || document.documentElement.scrollTop 
+    || document.body.scrollTop 
+    || 0;
+}
+
+export const getScrollLeft = (): number => {
+  return window.pageXOffset 
+    || document.documentElement.scrollLeft 
+    || document.body.scrollLeft 
+    || 0;
+}
+
+export const getScrollHeight = (): number => {
+  return document.documentElement.scrollHeight || document.body.scrollHeight;
+}
+
+export const getScrollWidth = (): number => {
+  return document.documentElement.scrollWidth || document.body.scrollWidth;
+}
+
+export const getAbsolutePosition = (relativePos: Point) => {
+  return new Point(relativePos.x += getScrollLeft(), relativePos.y += getScrollTop());
 }
