90f75f6c6fe7f391eadd06432ab3288c5f1b3b6c
heyitaki <akshath@berkeley.edu>
2020-09-19 17:48:03 -0700

    add get1, fix area param

--- src/utils/chrome/storage.ts
+++ src/utils/chrome/storage.ts
@@ -15,7 +15,7 @@ export interface CS {
 type AreaName = 'local' | 'sync' | 'managed';
 
 /**
- * Get values corresponding to given keys from local storage. This method takes in a single key, 
+ * Get values corresponding to given keys from chrome storage. This method takes in a single key, 
  * a list of keys, or an object containing the keys mapped to their default values, and returns a
  * promise which returns an object containing the key-value pairs retrieved from storage. `null` 
  * can be passed in to retrieve all stored key-value pairs.
@@ -30,10 +30,10 @@ type AreaName = 'local' | 'sync' | 'managed';
  * @param key
  * @param area
  */
-export function get<K extends keyof CS>(key: null): Promise<{[k in K]: CS[k]}>;
-export function get<K extends keyof CS>(key: K): Promise<{[key in K]: CS[key]}>;
-export function get<K extends keyof CS>(key: K[]): Promise<{[key in K]: CS[key]}>;
-export function get<J extends K, K extends keyof CS>(key: Partial<{[k in K]: CS[k]}>): Promise<{[j in J]: CS[j]}>;
+export function get<K extends keyof CS>(key: null, area?: AreaName): Promise<{[k in K]: CS[k]}>;
+export function get<K extends keyof CS>(key: K, area?: AreaName): Promise<{[key in K]: CS[key]}>;
+export function get<K extends keyof CS>(key: K[], area?: AreaName): Promise<{[key in K]: CS[key]}>;
+export function get<J extends K, K extends keyof CS>(key: Partial<{[k in K]: CS[k]}>, area?: AreaName): Promise<{[j in J]: CS[j]}>;
 export function get<K extends keyof CS>(key: K | K[] | Partial<{[k in K]: CS[k]}>, area: AreaName='local') {
   return new Promise((resolve, reject) => {
     chrome.storage[area].get(key, (items) => {
@@ -49,8 +49,18 @@ export function get<K extends keyof CS>(key: K | K[] | Partial<{[k in K]: CS[k]}
 }
 
 /**
- * Set given key-value pairs in chrome.storage.local.
+ * Get value corresponding to given key.
+ * @param key 
+ * @param area 
+ */
+export function get1<K extends keyof CS>(key: K, area: AreaName='local'): Promise<CS[K]> {
+  return get(key, area).then((items) => items[key]);
+}
+
+/**
+ * Set given key-value pairs in chrome storage.
  * @param items
+ * @param area 
  */
 export function set<K extends keyof CS>(items: Partial<{[k in K]: CS[k]}>, area: AreaName='local'): Promise<void> {
   return new Promise((resolve, reject) => {
@@ -67,8 +77,9 @@ export function set<K extends keyof CS>(items: Partial<{[k in K]: CS[k]}>, area:
 }
 
 /**
- * Remove given key or list of keys from chrome.storage.local.
+ * Remove given key or list of keys from chrome storage.
  * @param keys
+ * @param area 
  */
 export function remove<K extends keyof CS>(keys: K | K[], area: AreaName='local'): Promise<void> {
   return new Promise((resolve, reject) => {
@@ -85,7 +96,8 @@ export function remove<K extends keyof CS>(keys: K | K[], area: AreaName='local'
 }
 
 /**
- * Clear chrome.storage.local.
+ * Clear chrome storage.
+ * @param area 
  */
 export function clear(area: AreaName='local'): Promise<void> {
   return new Promise((resolve, reject) => {
