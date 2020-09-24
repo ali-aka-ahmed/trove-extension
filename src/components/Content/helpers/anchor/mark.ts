e7c92c863d961bd02b5c971c07430a3ef313e23e
heyitaki <akshath@berkeley.edu>
2020-09-24 16:28:51 -0700

    add mark works

--- src/components/Content/helpers/anchor/mark.ts
+++ src/components/Content/helpers/anchor/mark.ts
@@ -1,52 +1,103 @@
-export const mark = (range: Range) => {
+import { v4 as uuid } from 'uuid';
+
+/**
+ * Ids of different types of marks. Making the assumption we can only have one instance of each
+ * type visiable at once.
+ */
+export enum MarkId {
+  Post    = 'TbdMark__PostAnchor',
+  NewPost = 'TbdMark__NewPostAnchor'
+}
+
+export enum MarkDataKey {
+  FirstMarkId = 'tbdProgenitorId',
+  ThisMarkId  = 'tbdId',
+  NextMarkId  = 'tbdSuccessorId'
+};
+
+export const mark = (range: Range, id: MarkId, color?: string) => {
   let start = range.startContainer;
   let end = range.endContainer;
+  let hasContent = true;
 
   // Determine end
-  if (range.endOffset === 0) {
-    // There is no included content in this node, so prune it from the range
+  if (range.endOffset === 0) { 
     while (!end.previousSibling && end.parentNode !== range.commonAncestorContainer) {
       end = end.parentNode!;
     }
 
-    if (end.previousSibling) end = end.previousSibling;
-  } else if (end.nodeType === Node.TEXT_NODE && range.endOffset < (end.nodeValue?.length || 0)) {
-    // Remove text after offset
-    (end as Text).splitText(range.endOffset);
-  } else if (range.endOffset <= end.childNodes.length) {
-    // Use node specified by end offset (exclusive)
+    end = end.previousSibling!;
+  } else if (end.nodeType === Node.TEXT_NODE) {
+    if (range.endOffset < end.nodeValue!.length) {
+      (end as Text).splitText(range.endOffset);
+    }
+  } else if (range.endOffset > 0) {
     end = end.childNodes.item(range.endOffset - 1);
   }
 
   // Determine start
-  const isTextNode = start.nodeType === Node.TEXT_NODE;
-  const bound = isTextNode ? (start as Text).nodeValue?.length || 0 : start.childNodes.length;
-  const hasContent = range.startOffset !== bound;
-  if (!hasContent) {
-    // Do nothing because there is no included content in this node
-  } else if (isTextNode && range.startOffset > 0) {
-    // Remove text before offset
-    start = (start as Text).splitText(range.startOffset);
-    if (end === start.previousSibling) start = end; // TODO: do we need this?
+  if (start.nodeType === Node.TEXT_NODE) {
+    if (range.startOffset === start.nodeValue!.length) {
+      hasContent = false;
+    } else if (range.startOffset > 0) {
+      start = (start as Text).splitText(range.startOffset);
+
+      if (end === start.previousSibling) {
+        end = start;
+      }
+    }
   } else if (range.startOffset < start.childNodes.length) {
-    // Use node specified by end offset (inclusive)
     start = start.childNodes.item(range.startOffset);
+  } else {
+    hasContent = false;
   }
 
-  // TODO: do we need this?
-  range.setStart(range.startContainer, 0);
-  range.setEnd(range.startContainer, 0);
-
   // This is where the magic happens
-  const elements: Node[] = [];
+  const marks: HTMLElement[] = [];
   for (let done = false, node = start; !done; ) {
     if (hasContent && node.nodeType === Node.TEXT_NODE && !isTable(node)) {
       let wrapper = node.previousSibling;
-      if (!wrapper || elements.length === 0 || wrapper !== elements[elements.length - 1]) {
-        // Add wrapper
-        const element = document.createElement('mark');
-        if (elements.length === 0 && )
+      if (!wrapper || marks.length === 0 || wrapper !== marks[marks.length - 1]) {
+        // Create next mark in chain
+        const mark = document.createElement('mark'); 
+        mark.dataset[MarkDataKey.ThisMarkId] = uuid();
+        if (marks.length === 0) {
+          mark.id = id;
+        } else {
+          mark.dataset[MarkDataKey.FirstMarkId] = marks[0].id;
+          const prevMark = marks[marks.length - 1];
+          prevMark.dataset[MarkDataKey.NextMarkId] = mark.dataset[MarkDataKey.ThisMarkId];
+        }
+
+        marks.push(mark);
+
+        // Position new wrapper accordingly
+        wrapper = mark;
+        node.parentNode?.insertBefore(wrapper, node);
       }
+
+      // Move node under wrapper
+      wrapper.appendChild(node);
+      node = wrapper.lastChild!;
+      hasContent = false;
+    }
+
+    if (node === end && (!hasContent || !end.hasChildNodes())) {
+      done = true;
+    }
+
+    if (isTerminal(node)) {
+      hasContent = false;
+    }
+
+    if (hasContent && node.firstChild) {
+      node = node.firstChild!;
+    } else if (node.nextSibling) {
+      node = node.nextSibling;
+      hasContent = true;
+    } else {
+      node = node.parentNode!;
+      hasContent = false;
     }
   }
 }
@@ -59,4 +110,25 @@ const isTable = (node: Node) => {
     HTMLTableSectionElement
   ];
   return types.some(type => node.parentNode instanceof type);
+}
+
+const isTerminal = (node: Node) => {
+  const types = [
+    HTMLScriptElement,
+    HTMLStyleElement,
+    HTMLSelectElement
+  ];
+  return types.some(type => node.parentNode instanceof type);
+}
+
+type valueof<T> = T[keyof T];
+
+/**
+ * Add `data-` prefix and kebab-case given `element.dataset` key to construct data attribute name.
+ * 
+ * Ex: `'tbdId' => 'data-tbd-id'`
+ * @param attr 
+ */
+const attr = (attr: valueof<typeof MarkDataKey>) => {
+  return 'data-' + attr.replace(/([A-Z])/g, '-$1').toLowerCase();
 }
