f8131d087b3c6a1b8d28e583440af07f03bb9930
Ali Ahmed <ali.ahmed@berkeley.edu>
2020-09-25 16:55:11 -0700

    removed more references, still erroring due to type definition of Anchor

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,7 +1,7 @@
 export { default as Node } from './labels/Node';
 export { default as Relationship } from './labels/Relationship';
 export { Notification } from './nodes/Notification';
-export { Post, TaggedUser } from './nodes/Post';
+export { Anchor, AnchorType, Point, Post, TaggedUser } from './nodes/Post';
 export { User } from './nodes/User';
 export { UserPrivate } from './nodes/UserPrivate';
 export { BaseRes } from './routes';
@@ -10,3 +10,4 @@ export {
   UsernameReqBody, UsernameRes
 } from './routes/auth';
 export { GetUserReqParams, UpdateUserReqBody, UpdateUserReqParams, UserRes } from './routes/user';
+
