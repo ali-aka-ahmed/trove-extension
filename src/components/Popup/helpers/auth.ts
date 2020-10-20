99b04f50d93d3373d4ac0c0071d59410eb6d8997
Ali Ahmed <ali.ahmed@berkeley.edu>
2020-10-20 16:08:06 -0700

    added forgot password

--- src/components/Popup/helpers/auth.ts
+++ src/components/Popup/helpers/auth.ts
@@ -1,4 +1,4 @@
-import { LoginReqBody } from '../../../server/auth';
+import { ForgotReqBody, LoginReqBody } from '../../../server/auth';
 
 export const validateUsername = (username: string): { success: boolean, message?: string } => {
   if (username.length === 0) {
@@ -39,4 +39,12 @@ export const createLoginArgs = (loginParam: string, password: string): LoginReqB
   if (emailRegex.test(loginParam)) return { email: loginParam, password };
   if (phoneNumberRegex.test(loginParam)) return { phoneNumber: loginParam, password };
   else return { username: loginParam, password };
+}
+
+export const createForgotPasswordArgs = (forgotPasswordParam: string): ForgotReqBody | null => {
+  const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+  const phoneNumberRegex = /(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})/;
+  if (emailRegex.test(forgotPasswordParam)) return { email: forgotPasswordParam };
+  if (phoneNumberRegex.test(forgotPasswordParam)) return { phoneNumber: forgotPasswordParam };
+  else return null;
 }
--- webpack.common.js
+++ webpack.common.js
@@ -71,6 +71,13 @@ module.exports = {
           }
         ]
       },
+      {
+        test: /\.(png|jpe?g|gif|jp2|webp)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'images/[name].[ext]'
+        }
+      },
     ]
   },
   resolve: {
