12dbe995bda20529118b33c940762eb20bc79683
Ali Ahmed <ali.ahmed@berkeley.edu>
2020-10-22 14:12:12 -0700

    bugfixes

--- src/components/Content/Sidebar/index.tsx
+++ src/components/Content/Sidebar/index.tsx
@@ -287,8 +287,14 @@ export default function Sidebar() {
     
     // Listener to update extension-wide settings
     chrome.storage.onChanged.addListener((change) => {
-      if (change.isExtensionOn !== undefined) setIsExtensionOn(change.isExtensionOn.newValue);
-      if (change.user !== undefined) setUser(new User(change.user.newValue));
+      if (change.isExtensionOn !== undefined) {
+        if (change.user.newValue) setIsExtensionOn(change.isExtensionOn.newValue);
+        else setIsExtensionOn(false);
+      }
+      if (change.user !== undefined) {
+        if (change.user.newValue) setUser(new User(change.user.newValue));
+        else setUser(null)
+      }
     });
   }, []);
 
