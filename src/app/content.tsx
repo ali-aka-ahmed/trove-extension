544acfc6728f85306265c7caa74842f5fd01ebca
heyitaki <akshath@berkeley.edu>
2020-11-06 15:44:47 -0800

    remove sidebar for now

--- src/app/content.tsx
+++ src/app/content.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SidebarWrapper from '../components/SidebarWrapper';
 import TooltipWrapper from '../components/TooltipWrapper';
 
 // Render sidebar
-const sidebarContainer = document.createElement('div');
-sidebarContainer.setAttribute('class', 'TbdSidebarWrapper');
-document.body.appendChild(sidebarContainer);
+// const sidebarContainer = document.createElement('div');
+// sidebarContainer.setAttribute('class', 'TbdSidebarWrapper');
+// document.body.appendChild(sidebarContainer);
 
-const sidebarShadowRoot = sidebarContainer.attachShadow({ mode: 'open' });
-ReactDOM.render(<SidebarWrapper />, sidebarShadowRoot);
+// const sidebarShadowRoot = sidebarContainer.attachShadow({ mode: 'open' });
+// ReactDOM.render(<SidebarWrapper />, sidebarShadowRoot);
 
 // Render tooltip
 // TODO: convert shadow host to React component so we can remove entire tooltip
