be8de2895452b0ed614ea6ce358ed38898cc1d2b
heyitaki <akshath@berkeley.edu>
2020-11-09 12:53:00 -0800

    input looks like button

--- src/components/TooltipWrapper/Tooltip/InputPill/index.tsx
+++ src/components/TooltipWrapper/Tooltip/InputPill/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from 'react';
 
 interface InputPillProps {
   
@@ -6,15 +6,22 @@ interface InputPillProps {
 
 export default function InputPill(props: InputPillProps) {
   const [isInput, setIsInput] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const onClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     setIsInput(true);
   }
 
+  useEffect(() => {
+    if (isInput) {
+      (inputRef.current as HTMLInputElement).focus();
+    }
+  }, [isInput])
+
   return (
     <>
       {isInput ? (
-        <input className="TbdInputPill TbdInputPill--input" />
+        <input className="TbdInputPill TbdInputPill--input" ref={inputRef} />
       ) : (
         <button className="TbdInputPill TbdInputPill--button" onClick={onClick}>+ Topic</button>
       )}
