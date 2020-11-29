0b000dbf6d27f38d07501d785992f43c74030848
Ali Ahmed <ali.ahmed@berkeley.edu>
2020-11-29 12:28:36 -0500

    when done with a prod version, we will have this on. We will then just use dev and branches for development

---
+++ src/config/example.tsx
@@ -0,0 +1,4 @@
+export const LOGGING: boolean = false;
+export const ORIGIN = 'https://www.trove.so';
+export const BACKEND_URL = 'https://api.trove.so';
+export const VALID_DOMAINS = ['www.trove.so']
