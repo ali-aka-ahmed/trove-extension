e8e5caa560eb188187278d592eb8a837f89f07d6
Ali Ahmed <ali.ahmed@berkeley.edu>
2021-01-19 14:47:29 -0800

    some styling things were not correctly refactored when making user info showing 'optional'

--- src/components/TooltipWrapper/Tooltip/index.tsx
+++ src/components/TooltipWrapper/Tooltip/index.tsx
@@ -325,7 +325,7 @@ export default function Tooltip(props: TooltipProps) {
     );
 
     return (
-      <div className="TbdTooltip__TopicList">
+      <div className={`${post && (!post.topics || post.topics.length === 0) ? '' : "TbdTooltip__TopicList"}`}>
         {!post && <InputPill onSubmit={addTopic} style={{ marginBottom: '3px' }} />}
         {pills}
       </div>
